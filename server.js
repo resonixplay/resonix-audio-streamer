1f5e08eb32a9ada195551096a9708ff81f5e69d1
Ravi <iamrks@outlook.com>
2026-01-14 09:53:35 +0530

    added urls

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
  */
 app.use(
   cors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'http://localhost:4200', 'https://resonixplay.github.io', 'https://resoninx.web.app'],
     methods: ['GET'],
     credentials: true
   })
